0a11e9b79c1505316e88b69d4b4cab2638635c22
GabrielaFuentes <gabrielafuentes21@gmail.com>
2024-08-20 19:40:11 -0300

    Implementacion de paginacion

--- src/models/carts.model.js
+++ src/models/carts.model.js
@@ -1,13 +1,16 @@
 import mongoose from "mongoose";
 
-
 const cartsColletions = "carts";
 
 const cartsSchema = new mongoose.Schema({
     timestamp: { type: String, required: true },
-    products: { type: Array, required: true },
-})
-
+    products: [
+        {
+            productId: { type: mongoose.Schema.Types.ObjectId, ref: "products" },
+            quantity: { type: Number, default: 1 }
+        }
+    ],
+});
 
 const cartsModel = mongoose.model(cartsColletions, cartsSchema);
 
